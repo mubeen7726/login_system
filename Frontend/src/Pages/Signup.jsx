45b7ad64088837ca7e3222d5c98560305ce7459a
mubeen7726 <mubeen772630@gmail.com>
2025-02-09 20:33:43 +0000

    Update Signup.jsx

--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
       return handleError("Name, email, and password are required");
     }
     try {
-      const url = "http://localhost:8080/auth/signup";
+      const url = "https://login-system-alpha-ten.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
